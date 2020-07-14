3e51f8c511affc8dc14afd2dee7a56c5139ca264
Linh Tran <ltran10@fb.com>
2020-07-14 10:19:04 -0700

    Fix UI and create Add new Item layout

--- app/src/main/java/com/example/buckos/activities/ItemDetailsActivity.java
+++ app/src/main/java/com/example/buckos/activities/ItemDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.buckos.R;
+
 public class ItemDetailsActivity extends AppCompatActivity {
 
     @Override
